753249b6d7a04a02b0d2c98bf97d4c17245ac96a
QuickAirMenu <QuickAirMenu@gmail.com>
2025-04-04 02:32:02 +0000

    تم إضافة التحديثات إلى المشروع

---
+++ update_repo.sh
@@ -0,0 +1,10 @@
+
+
+#!/bin/bash
+# الانتقال إلى مجلد المشروع
+cd /home/u227212075/domains/airmenu.site/public_html/menu-test || exit
+
+# سحب التحديثات من مستودع GitHub
+git pull https://github.com/QuickAirMenu/imenu-system.git main
+
+
